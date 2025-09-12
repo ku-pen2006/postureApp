163dc1994f6c22d7341a571f470ce893ee055976
shiba <shibamomo.3344@icloud.com>
2025-09-12 17:24:51 +0900

    ミーヤキャットの画像

--- postureApp/KawausoView.swift
+++ postureApp/KawausoView.swift
@@ -2,21 +2,20 @@ import SwiftUI
 
 struct KawausoView: View {
     var body: some View {
-            VStack {
-                // カワウソの画像
-                Image("kawauso")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .foregroundColor(.brown)
-                    .padding()
+        VStack {
+            // カワウソの画像
+            Image("kawauso")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .padding()
 
-                // メッセージ
-                Text("姿勢が悪くなっていますよ！")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .padding(.top, 16)
-            }
-            .frame(width: 400, height: 400) // ウィンドウのサイズ
+            // メッセージ
+            Text("姿勢が悪くなっていますよ！")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .padding(.top, 16)
+        }
+        .frame(width: 400, height: 400) // ウィンドウのサイズ
     }
 }
